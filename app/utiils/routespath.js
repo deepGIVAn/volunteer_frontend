7334665d5f1b7568884e6ccd4d9e14f14e25295e
deepGIVAn <deepakpvt26@gmail.com>
2025-08-25 00:42:03 +0530

    del

--- app/utiils/routespath.js
+++
@@ -1,9 +0,0 @@
-export const routesPath = {
-	home: '/',
-	admin: '/admin',
-	login: '/login',
-	adminOrganisations: '/admin/organisations',
-	adminVolunteers: '/admin/volunteers',
-	adminRecycleBin: '/admin/recycle-bin',
-	adminRoles: '/admin/roles',
-}
